046cd627bfea4f5d5bdd386904fbd189a27a2130
qubinakarmi <qubinakarmi@gmail.com>
2025-10-01 16:53:27 +0545

    add style to about.view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,9 +4,10 @@
 
         <h1 class="text-center">This is about page </h1>
 
-        <p>My name is Qubi Nakarmi and I am from Madhyapur Thimi. I am Laravel Developer</p>
-
-        <button class="btn btn-outline-info">Please click me</button>
+        <h2 class="mx-5 justify-center" style="color: rgb(255, 128, 0);">My name is Qubi Nakarmi and I am from Madhyapur Thimi. 
+       <br>
+ <span style="color:rgba(255, 0, 0, 0.721);">I am Laravel Developer</span>   </h2> 
+        <button class="btn btn-outline-info mx-5">Please click me</button>
     </x-slot>
 </x-layout>
 
