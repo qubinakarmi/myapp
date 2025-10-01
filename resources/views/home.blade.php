1596c479808ebac5b7c24bbc265464bb35dd9980
qubinakarmi <qubinakarmi@gmail.com>
2025-10-01 11:22:38 +0545

    4th commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,8 @@
 
          Home page look like this 
          Hello from Qubi Nakarmi
+
+         <button>SubmitS</button>
     </x-slot>
         <x-slot name="main"><h1>home page</h1></x-slot>
 </x-layout>
